9514361d9ad833403ee1e9b6538c5a73ea1d29cc
MironBerch <mironberchetov@gmail.com>
2023-03-18 23:14:48 +0300

    add accounts test view

---
+++ register_of_talented_students/accounts/tests/test_views.py
@@ -0,0 +1,20 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class TestSignupView(TestCase):
+
+    def test_signup_view_get_method(self):
+        client = Client()
+        response = client.get(reverse('signup'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/signup.html')
+        
+
+class TestSigninView(TestCase):
+
+    def test_signin_view_get_method(self):
+        client = Client()
+        response = client.get(reverse('signin'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/signin.html')
