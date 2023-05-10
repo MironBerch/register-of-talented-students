a4b14fc5511a992e9b62e6173af31a34c0599f0a
MironBerch <mironberchetov@gmail.com>
2023-05-10 23:58:45 +0300

    Add postgres in docker

--- register_of_talented_students/config/settings.py
+++ register_of_talented_students/config/settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': environ.get('POSTGRES_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': environ.get('POSTGRES_DB', BASE_DIR / 'db.sqlite3'),
+        'USER': environ.get('POSTGRES_USER', 'user'),
+        'PASSWORD': environ.get('POSTGRES_PASSWORD', 'password'),
+        'HOST': environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
---
+++ register_of_talented_students/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Postgres not yet run"
+
+    # Проверяем доступность хоста и порта
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 0.1
+    done
+
+    echo "Postgres did run"
+fi
+
+python manage.py flush --no-input
+
+python manage.py migrate
+
+exec "$@"
